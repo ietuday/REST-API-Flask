dfb6074c05b11f36c8a9ef9b87745f71c4659eba
Udayaditya Singh <udayaditya.singh@gmail.com>
2020-02-05 01:42:53 +0530

    "changes"

---
+++ test/deserializing.py
@@ -0,0 +1,20 @@
+from marshmallow import Schema, fields, INCLUDE, EXCLUDE
+
+
+class BookSchema(Schema):
+    title = fields.Str()
+    author = fields.Str()
+
+
+incoming_book_data = {
+    "title": "Clean Code",
+    "author": "Bob Martin",
+    "description": "A book about writing cleaner code, with examples in Java",
+}
+
+
+book_schema = BookSchema(unknown=EXCLUDE)
+
+book = book_schema.load(incoming_book_data)
+
+print(book)
